ee8b4c86e984ceb3795b8e4790bbdef6200be759
chetan-redkivi <chetan.tomar@redkivi.com>
2012-06-23 12:11:34 +0530

    test time zone

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,26 +5,27 @@ class HomeController < ApplicationController
 			feed = @graph.get_connections("me", "feed")
 			@friends_profile = @graph.get_connections("me", "friends", "fields"=>"name,birthday,gender")
 			profile = @graph.get_object("me")
+#			render :text =>profile.inspect and return false
 			session[:image]= @graph.get_picture("1310835663",:type=>"large")
 			current_date = Date.today.strftime('%m-%d-%Y').split('-')
 			@today_birthday = []
 			@result = []
 			upcomming = current_date[1].to_i+5
-			@friends_profile.each do |friend|
-				if !friend["birthday"].nil?
-					birthday = friend["birthday"].split('/')
-					if current_date[0] == birthday[0]
-						#month is same
-						if current_date[1]==birthday[1]
-							#Date is same
-							@today_birthday << friend["id"]
-						end
-					end
-				end
-			end
-			@today_birthday.each do |id|
-				@graph.put_wall_post("Wishing you a very special Birthday", {}, id)
-			end
+#			@friends_profile.each do |friend|
+#				if !friend["birthday"].nil?
+#					birthday = friend["birthday"].split('/')
+#					if current_date[0] == birthday[0]
+#						#month is same
+#						if current_date[1]==birthday[1]
+#							#Date is same
+#							@today_birthday << friend["id"]
+#						end
+#					end
+#				end
+#			end
+#			@today_birthday.each do |id|
+#				@graph.put_wall_post("Wishing you a very special Birthday", {}, id)
+#			end
 	  end
 	end
 end
