8796b647f48b3ea8af3534076f79a6755d736032
chetan-redkivi <chetan.tomar@redkivi.com>
2012-06-23 14:17:14 +0530

    timezone issue resolved

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,20 +2,20 @@ class HomeController < ApplicationController
   def index
 		if !session[:access_token].nil?
 			@graph = Koala::Facebook::API.new(session[:access_token])
-			feed = @graph.get_connections("me", "feed")
 			@friends_profile = @graph.get_connections("me", "friends", "fields"=>"name,birthday,gender")
-			profile = @graph.get_object("me")
+			@profile = @graph.get_object("me")
 			session[:image]= @graph.get_picture("me",:type=>"large")
-			current_date = DateTime.now.new_offset(profile["timezone"]/24).strftime('%m-%d-%Y').split('-')
+			@current_date = DateTime.now.new_offset(@profile["timezone"]/24).strftime('%m-%d-%Y').split('-')
+	#		render :text => @current_date.inspect and return false
 			@today_birthday = []
 			@result = []
-			upcomming = current_date[1].to_i+5
+			@upcomming = @current_date[1].to_i+10
 #			@friends_profile.each do |friend|
 #				if !friend["birthday"].nil?
 #					birthday = friend["birthday"].split('/')
-#					if current_date[0] == birthday[0]
+#					if @current_date[0] == birthday[0]
 #						#month is same
-#						if current_date[1]==birthday[1]
+#						if @current_date[1]==birthday[1]
 #							#Date is same
 #							@today_birthday << friend["id"]
 #						end
