23a983d0f7a3c269e2e4a3548aca7e3bf4498a05
chetan-redkivi <chetan.tomar@redkivi.com>
2012-06-27 14:43:40 +0530

    test final

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,7 @@ class HomeController < ApplicationController
 #  	render :text => params.inspect and return false
 		@feedback = Feedback.new
 		@feedbacks = Feedback.find(:all)
-		@testinomial = Feedback.find(:first)
-		render :text => @testinomial.inspect and return false
+		@testinomial = Feedback.find(1)
 		@json = Location.all.to_gmaps4rails
 		if !session[:access_token].nil?
 			@graph = Koala::Facebook::API.new(session[:access_token])
