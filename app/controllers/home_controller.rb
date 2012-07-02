30b909d9716c52114564f1bc9988f8af0f78085d
chetan-redkivi <chetan.tomar@redkivi.com>
2012-07-02 12:21:58 +0530

    image send on facebook wall test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-#  	render :text => params.inspect and return false
+#  	render :text =>  (Rails.root).join("#{Myavatar.find([1,6,11,15,19].sample).avatar.url}").inspect and return false
 
 		@feedback = Feedback.new
 		@feedbacks = Feedback.find(:all)
@@ -8,6 +8,12 @@ class HomeController < ApplicationController
 		@json = Location.all.to_gmaps4rails
 
 		if !session[:access_token].nil?
+
+			$today_birthday_ids = []
+			@result = []
+			@today_birthday = []
+			@next_month_bday=[]
+			@nxt_result=[]
 
 			@graph = Koala::Facebook::API.new(session[:access_token])
 
@@ -21,11 +27,6 @@ class HomeController < ApplicationController
 			@current_date = DateTime.now.new_offset(@profile["timezone"]/24).strftime('%m-%d-%Y').split('-')
 			@total_days = (Date.new(Time.now.year,12,31).to_date<<(12-(DateTime.now.strftime('%m')).to_i)).day
 			@nxt_month_total_days = (Date.new(Time.now.year,12,31).to_date<<(12-(DateTime.now.strftime('%m')).to_i+1)).day
-			@today_birthday_ids = []
-			@result = []
-			@today_birthday = []
-			@next_month_bday=[]
-			@nxt_result=[]
 			@upcomming = @current_date[1].to_i+10
 			#render :text => @friends_profile.inspect and return false
 			@friends_profile.each do |friend|
@@ -35,7 +36,7 @@ class HomeController < ApplicationController
 						#month is same
 						if @current_date[1]==birthday[1]
 							#Date is same
-							@today_birthday_ids << friend["id"]
+							$today_birthday_ids << friend["id"]
 							@today_birthday <<  {"name" => friend["name"],"birthday" => birthday[1],"id" => friend["id"]}
 						end
 						if birthday[1].to_i > @current_date[1].to_i && birthday[1].to_i < @upcomming
@@ -53,26 +54,31 @@ class HomeController < ApplicationController
 				@nxt_result = @nxt_result.sort_by {|hsh| hsh["birthday"]}
 				@result = @result+@nxt_result
 				@next_month_bday = @next_month_bday.sort_by { |hsh| hsh["birthday"] }
-			if !params["defaultMsz"].nil?
-				@today_birthday_ids.each do |id|
-				@graph.put_wall_post("Wishing you a very special Birthday", {}, id)
-#				@graph.put_picture("/home/viinfo/Downloads/facebook.png", { "message" => "This is the photo caption" }, "id")
-
-				end
-				flash[:notice] = ""
-			end
-			if !params["customMsz"].nil?
-				if !params["customMsz"].blank?
-					@today_birthday_ids.each do |id|
-						@graph.put_wall_post("#{params["customMsz"]}", {}, id)
+				if !params["customMsz"].nil?
+					if !params["customMsz"].blank?
+						@today_birthday_ids.each do |id|
+							@graph.put_wall_post("#{params["customMsz"]}", {}, id)
+						end
+					else
+						flash[:notice] = "Please Enter Your Quote it Should not Be blank :)"
 					end
-				else
-					flash[:notice] = "Please Enter Your Quote it Should not Be blank :)"
 				end
-
-			end
 	  end
 	end
+
+
+	def defaultMsz
+		if !$today_birthday_ids.blank?
+			$today_birthday_ids.each do |id|
+				@graph.put_wall_post("Wishing you a very special Birthday", {}, id)
+				image_link = Myavatar.find((1..8).to_a.sample).avatar.url
+				@graph.put_picture("#{(Rails.root).join(image_link)}", { "message" => "Wishing you a very special Birthday" }, id)
+			end
+			flash[:notice] = ""
+		end
+		redirect_to "/"
+	end
+
 
 	def facefeed
 		@graph = Koala::Facebook::API.new(session[:access_token])
